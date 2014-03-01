729a61b85166521cd2402fbd3fa8caed4f1aa0f7
DidierStevens <didier.stevens@gmail.com>
2014-03-01 01:17:23 +0100

    Update punbup.py
    Added dump options

--- punbup.py
+++ punbup.py
@@ -151,6 +151,100 @@ def getHashes(bupname,htype):
         print >>sys.stderr, 'Error - %s' % e
         sys.exit(1)
         
+dumplinelength = 16
+
+# CIC: Call If Callable
+def CIC(expression):
+    if callable(expression):
+        return expression()
+    else:
+        return expression
+
+# IFF: IF Function
+def IFF(expression, valueTrue, valueFalse):
+    if expression:
+        return CIC(valueTrue)
+    else:
+        return CIC(valueFalse)
+
+class cDumpStream():
+    def __init__(self):
+        self.text = ''
+
+    def Addline(self, line):
+        if line != '':
+            self.text += line + '\n'
+
+    def Content(self):
+        return self.text
+
+def HexDump(data):
+    oDumpStream = cDumpStream()
+    hexDump = ''
+    for i, b in enumerate(data):
+        if i % dumplinelength == 0 and hexDump != '':
+            oDumpStream.Addline(hexDump)
+            hexDump = ''
+        hexDump += IFF(hexDump == '', '', ' ') + '%02X' % ord(b)
+    oDumpStream.Addline(hexDump)
+    return oDumpStream.Content()
+
+def CombineHexAscii(hexDump, asciiDump):
+    if hexDump == '':
+        return ''
+    return hexDump + '  ' + (' ' * (3 * (16 - len(asciiDump)))) + asciiDump
+
+def HexAsciiDump(data):
+    oDumpStream = cDumpStream()
+    hexDump = ''
+    asciiDump = ''
+    for i, b in enumerate(data):
+        if i % dumplinelength == 0:
+            if hexDump != '':
+                oDumpStream.Addline(CombineHexAscii(hexDump, asciiDump))
+            hexDump = '%08X:' % i
+            asciiDump = ''
+        hexDump+= ' %02X' % ord(b)
+        asciiDump += IFF(ord(b) >= 32 and ord(b), b, '.')
+    oDumpStream.Addline(CombineHexAscii(hexDump, asciiDump))
+    return oDumpStream.Content()
+
+def IdentityFunction(data):
+    return data
+
+#Fix for http://bugs.python.org/issue11395
+def StdoutWriteChunked(data):
+    while data != '':
+        sys.stdout.write(data[0:10000])
+        sys.stdout.flush()
+        data = data[10000:]
+
+def printDump(bupname, DumpFunction=IdentityFunction, allfiles=False):
+    #
+    #Print Hex dump/Hex-ASCII dump of first or all streams
+    #
+    if sys.platform == 'win32' and DumpFunction == IdentityFunction:
+        import msvcrt
+        msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
+    try:
+        if OleFileIO_PL.isOleFile(bupname) is not True:
+            print >>sys.stderr, 'Error - %s is not a valid OLE file.' % bupname
+            sys.exit(1)
+
+        ole = OleFileIO_PL.OleFileIO(bupname)
+        printNewline = False
+        for entry in ole.listdir():
+            if entry[0] != "Details":
+                if printNewline:
+                    print
+                printNewline = True
+                StdoutWriteChunked(DumpFunction(decryptStream(ole.openstream(entry[0]).read())))
+                if not allfiles:
+                    break
+        ole.close()
+    except Exception as e:
+        print >>sys.stderr, 'Error - %s' % e
+        sys.exit(1)
 
 def main():
     parser = argparse.ArgumentParser(description="This script can be used to extract quarantined files from a McAfee .bup file. If run with no additional options the script will extract all files from the .bup and place them in a folder with the same name as the supplied .bup file.")
@@ -158,8 +252,13 @@ def main():
     parser.add_argument('-d','--details',dest="print_details",action='store_true',default=False,help="Only print the contents of the Details file. Don't extract any files.")
     parser.add_argument('-o','--original',dest="rename_original",action='store_true',default=False,help="Rename all quarantine files to their original names as noted in the Details file. Some assumptions have been made for this to feature to work. Use at your own risk.")
     parser.add_argument('-c','--hash',dest="hash",choices=('md5','sha1','sha256'),help="Calculates the hash for all of the files in the bup. ")
+    parser.add_argument('-f','--firstfile',dest="print_firstfile",action='store_true',default=False,help="Output the first quarantined file.")
+    parser.add_argument('-x','--hexdumpfirst',dest="print_hexdumpfirst",action='store_true',default=False,help="Perform a hexdump of the first quarantined file.")
+    parser.add_argument('-X','--hexdumpall',dest="print_hexdumpall",action='store_true',default=False,help="Perform a hexdump of all quarantined files.")
+    parser.add_argument('-a','--hexasciidumpfirst',dest="print_hexasciidumpfirst",action='store_true',default=False,help="Perform a hex & ASCII dump of the first quarantined file.")
+    parser.add_argument('-A','--hexasciidumpall',dest="print_hexasciidumpall",action='store_true',default=False,help="Perform a hex & ASCII dump of all quarantined files.")
     args = parser.parse_args()
-    
+
     bupname = args.infile
     #sanity check make sure .bup exists
     if not os.path.exists(bupname):
@@ -170,10 +269,20 @@ def main():
         print getDetails(bupname)
     elif args.hash:
         hashes = getHashes(bupname,args.hash)
-        for name in hashes:            
+        for name in hashes:
             print "%s hash for %s: %s" % (args.hash,name,hashes[name])
     elif args.rename_original:
         extractAll(bupname,original=True)
+    elif args.print_firstfile:
+        printDump(bupname)
+    elif args.print_hexdumpfirst:
+        printDump(bupname, HexDump)
+    elif args.print_hexdumpall:
+        printDump(bupname, HexDump, True)
+    elif args.print_hexasciidumpfirst:
+        printDump(bupname, HexAsciiDump)
+    elif args.print_hexasciidumpall:
+        printDump(bupname, HexAsciiDump, True)
     else:
         extractAll(bupname)
 
