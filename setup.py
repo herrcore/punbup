77d407f76fe9f021f625a9061306bebd8724cde5
idiom <sean@idiom.ca>
2015-04-30 18:04:24 -0400

    Fix issue with setup, Added py_modules argument

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(
     description='Python unbup script for McAfee .bup files - with some additional fun features. \
      Simple usage will extract all files from a .bup to a directory with the same name as the bup file.',
     install_requires=["olefile"],
+    py_modules=['punbup'],
     entry_points={'console_scripts': ['punbup=punbup:main']}
 )
